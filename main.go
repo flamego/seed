517253ce97d474567343ac962297290656a9d40a
Joe Chen <jc@unknwon.io>
2022-03-07 21:15:08 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Flamego. All rights reserved.
+// Copyright 2022 Flamego. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
